fd55115bbc665b675ebb75582b164edb03e57ed8
marcio lira <machollanda@gmail.com>
2020-04-03 23:48:28 -0300

    Textos da aula dois adicionados

---
+++ JS/main.js
@@ -0,0 +1,42 @@
+//AULA 1
+/**var nome = "Claudio holanda";
+var idade = 34;
+//var idade2 = 10;
+var idade2 = "10";
+var frase = "Japão é o melhor time do mundo";
+
+//alert ('Bem vindo' + nome);
+//alert (idade + idade2);
+
+//AQUI SERÁ IMPRESSO NO CONSOLE DO NAV
+//console.log(nome);
+//console.log(frase);
+
+//console.log(frase.replace("Japão", "Brasil"));
+//alert (frase.replace("Japão", "Brasil"));
+alert (frase.toLocaleUpperCase());
+//alert (frase.toLocaleLowerCase());
+*/
+
+//---------------------------
+//AULA 2
+
+var frutas = [{nome: "maça", cor: "vermelha"},{nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+/**var fruta = {nome: "maça", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "Pêra","Laranja"];
+            //POP SERVE PARA RETIRAR UM ITEM DA LISTA
+            //lista.push("uva");
+            //lista.pop("uva");
+            
+            //console.log(lista.length); MOSTRA O TAMANHO DO ARRAY
+            //console.log(lista.reverse()); MOSTRA O ARRAY COM O ÚLTIMO INCLUSO PRIMEIRO
+            //console.log(lista.toString[0]); AQUI IMPRIME O PRIMEIRO ELEMENTO DO ARRAY
+            //console.log(lista.toString()[0]); AQUI IMPRIME A PRIMEIRA LETRA DA PRIMEIRA PALAVRA
+            //console.log(lista.join(" - ")); //TRAZ ALGO DENTRO DA STRING
+            //console.log(lista.join(" - "));
